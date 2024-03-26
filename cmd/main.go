317acc0016dd21414ea4162a683a00832f88e045
babkin.denis2 <babkin.denis2@wb.ru>
2024-03-26 13:24:20 +0300

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -20,6 +20,17 @@ func initDB(host, port, user, password, dbname string) (*pgxpool.Pool, error) {
 	return db, err
 }
 
+type ServiceI interface {
+	myHandler(ctx *fiber.Ctx) error
+}
+type service struct {
+	db *pgxpool.Pool
+}
+
+func NewService(db *pgxpool.Pool) ServiceI {
+	s := service{db: db}
+	return &s
+}
 func main() {
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
@@ -34,36 +45,40 @@ func main() {
 	defer db.Close()
 
 	app := fiber.New()
+	s := NewService(db)
 
-	app.Get("/user/:id", func(c *fiber.Ctx) error {
-		id := c.Params("id")
-
-		var (
-			fio     string
-			avgBall float32
-			cipher  string
-		)
-
-		err = db.QueryRow(context.Background(), "SELECT fio, avg_ball, cipher FROM users WHERE id=$1", id).Scan(&fio, &avgBall, &cipher)
-		if err != nil {
-			if errors.Is(sql.ErrNoRows, err) {
-				return c.Status(fiber.StatusNotFound).SendString("Пользователь не найден")
-			}
-			log.Printf("Error querying database: %v", err)
-			return c.Status(fiber.StatusInternalServerError).SendString("Internal Server Error")
-		}
-
-		return c.JSON(fiber.Map{
-			"fio":      fio,
-			"avg_ball": avgBall,
-			"cipher":   cipher,
-		})
-	})
+	app.Get("/user/:id", s.myHandler)
 
 	appPort := os.Getenv("APP_PORT")
-	if port == "" {
-		port = "3000"
+	if appPort == "" {
+		appPort = "3000"
+	}
+	portStr := fmt.Sprintf(":%s", appPort)
+
+	log.Fatal(app.Listen(portStr))
+}
+
+func (s *service) myHandler(ctx *fiber.Ctx) error {
+	id := ctx.Params("id")
+
+	var (
+		fio     string
+		avgBall float32
+		cipher  string
+	)
+
+	err := s.db.QueryRow(context.Background(), "SELECT fio, avg_ball, cipher FROM users WHERE id=$1", id).Scan(&fio, &avgBall, &cipher)
+	if err != nil {
+		if errors.Is(sql.ErrNoRows, err) {
+			return ctx.Status(fiber.StatusNotFound).SendString("Пользователь не найден")
+		}
+		log.Printf("Error querying database: %v", err)
+		return ctx.Status(fiber.StatusInternalServerError).SendString("Internal Server Error")
 	}
 
-	log.Fatal(app.Listen(fmt.Sprintf(":%s", appPort)))
+	return ctx.JSON(fiber.Map{
+		"fio":      fio,
+		"avg_ball": avgBall,
+		"cipher":   cipher,
+	})
 }
